a692b6535ea96eeab34810eecad70b3d6408e9e1
Sowiekevinnurmitd <66833881+Sowiekevinnurmitd@users.noreply.github.com>
2020-11-13 09:45:51 +0100

    Update __init__.py

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -22,9 +22,7 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   CORS(app, resources={'/':{'origins': '*'}})
-'''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-'''
+
   # CORS HEADERS
   @app.after_request
   def after_request(response):
@@ -46,12 +44,7 @@ def create_app(test_config=None):
       'success': True,
       'categories': categories_dict
       })
-'''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
 
-'''
   @app.route('/questions')
   def retrieve_questions():
     selection = Question.query.order_by(Question.id).all()
@@ -73,18 +66,7 @@ def create_app(test_config=None):
     })
 
 
-'''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-'''
 
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
@@ -108,13 +90,7 @@ def create_app(test_config=None):
       abort(422)
 
 
-'''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-'''
   @app.route('/questions', methods=['POST'])
   def create_question():
   # this shows the request body 
@@ -141,16 +117,7 @@ def create_app(test_config=None):
   except: 
     abort(422)
 
-'''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-'''
   @app.route('/questions', methods=['POST'])
   def search_questions():
     try: 
@@ -168,16 +135,7 @@ def create_app(test_config=None):
       abort(422)
 
 
-'''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-'''
 
   @app.route('/categories/<int:id>/questions')
   def get_questions_by_category(id):
@@ -193,14 +151,7 @@ def create_app(test_config=None):
       'total_questions': len(Question.query.all()),
       'current_category': category.type
     })
-'''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-'''
 
   @app.route('/quizzes', methods=['POST'])
   def play_quiz_question():
@@ -255,17 +206,7 @@ def create_app(test_config=None):
     })
 
 
-'''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-'''
 
 
   @app.errorhandler(404)
